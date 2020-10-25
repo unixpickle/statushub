08cb51e1531747eb9993b11967bdf29bd0e25834
Alex Nichol <unixpickle@gmail.com>
2020-10-25 17:10:40 -0400

    basic fitbit app works

--- fitbit/app/index.js
+++ fitbit/app/index.js
@@ -1,15 +1,27 @@
 import document from "document";
 import * as messaging from "messaging";
 
-let statusText = document.getElementById("status");
-statusText.text = "Waiting...";
+const resultBox = document.getElementById('result-box');
+resultBox.text = 'Tap refresh.';
+
+const refreshButton = document.getElementById('refresh-button');
+refreshButton.addEventListener('click', (evt) => {
+  if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
+    resultBox.text = 'Loading...';
+    messaging.peerSocket.send({});
+  } else {
+    resultBox.text = 'Not connected to peer.';
+  }
+});
 
 messaging.peerSocket.addEventListener("open", (evt) => {
-  messaging.peerSocket.send({
-    "url": "https://ifconfig.me",
-  });
+  messaging.peerSocket.send({});
 });
 
 messaging.peerSocket.addEventListener("message", (evt) => {
-  statusText.text = JSON.stringify(evt.data);
+  if (evt.data['error']) {
+    resultBox.text = evt.data['error'];
+  } else {
+    resultBox.text = evt.data['data'];
+  }
 });
--- fitbit/companion/index.js
+++ fitbit/companion/index.js
@@ -2,15 +2,7 @@ import * as messaging from "messaging";
 import { settingsStorage } from "settings";
 
 messaging.peerSocket.addEventListener("message", (evt) => {
-  const requestURL = evt.data['url'];
-  fetch(requestURL).then((x) => x.text()).then((x) => {
-    const idx = x.indexOf('<strong id="ip_address">') || 0;
-    let ipAddr = x.substr(idx).split('>')[1].split('<')[0];
-    ipAddr += '/' + getBaseURL() + '/' + getPassword();
-    if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
-      messaging.peerSocket.send(ipAddr);
-    }
-  });
+  makeRequest();
 });
 
 function getBaseURL() {
@@ -19,4 +11,27 @@ function getBaseURL() {
 
 function getPassword() {
   return JSON.parse(settingsStorage.getItem('password') || '{"name":""}')["name"];
+}
+
+function makeRequest() {
+  const url = getBaseURL() + '/api/overview?password=' + encodeURIComponent(getPassword())
+  fetch(url, {
+        credentials: 'include',
+        redirect: 'manual',
+  }).then((x) => {
+    return x.json();
+  }).then((data) => {
+    if (data['error']) {
+      messaging.peerSocket.send({error: data['error']});
+    } else {
+      let buffer = '';
+      data['data'].forEach((x) => {
+        buffer += x['serviceName'] + '\n';
+        buffer += x['message'] + '\n---------------\n';
+      });
+      messaging.peerSocket.send({data: buffer});
+    }
+  }).catch((err) => {
+    messaging.peerSocket.send({error: 'Login error: ' + err});
+  });
 }
