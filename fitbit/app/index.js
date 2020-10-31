d0e87423b4c93986995716560d38ddd686b24f98
Alex Nichol <unixpickle@gmail.com>
2020-10-30 21:19:51 -0400

    fitbit: prepare for service log page

--- fitbit/app/index.js
+++ fitbit/app/index.js
@@ -1,53 +1,62 @@
 import document from "document";
 import * as messaging from "messaging";
 
-const resultBoxes = document.getElementsByClassName('result-box');
-const resultBoxTitles = document.getElementsByClassName('result-box-title');
-const refreshButton = document.getElementById('refresh-button');
+class OverviewPage {
+  constructor() {
+    this.resultBoxes = document.getElementsByClassName('result-box');
+    this.resultBoxTitles = document.getElementsByClassName('result-box-title');
+    this.refreshButton = document.getElementById('refresh-button');
+    
+    this.showMessage('Not connected to peer.');
 
-function clearResults() {
-  resultBoxes.forEach((x) => x.text = '');
-  resultBoxTitles.forEach((x) => x.text = '');
-}
-
-function showResults(results) {
-  clearResults();
-  results.forEach((x, i) => {
-    if (i < resultBoxes.length) {
-      resultBoxTitles[i].text = x[0];
-      resultBoxes[i].text = x[1];
-    }
-  });
-}
-
-function showMessage(msg) {
-  clearResults();
-  resultBoxes[0].text = msg;
-}
-
-function initialize() {
-  showMessage('Not connected to peer.');
-
-  refreshButton.addEventListener('click', (evt) => {
+    this.refreshButton.addEventListener('click', () => this.refresh());
+  }
+  
+  clearResults() {
+    this.resultBoxes.forEach((x) => x.text = '');
+    this.resultBoxTitles.forEach((x) => x.text = '');
+  }
+  
+  showResults(results) {
+    this.clearResults();
+    results.forEach((x, i) => {
+      if (i < this.resultBoxes.length) {
+        this.resultBoxTitles[i].text = x[0];
+        this.resultBoxes[i].text = x[1];
+      }
+    });
+  }
+  
+  showMessage(msg) {
+    this.clearResults();
+    this.resultBoxes[0].text = msg;
+  }
+  
+  refresh() {
     if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
-      showMessage('Loading...');
+      this.showMessage('Loading...');
       messaging.peerSocket.send({});
     } else {
-      showMessage('Not connected to peer.');
+      this.showMessage('Not connected to peer.');
     }
-  });
-
-  messaging.peerSocket.addEventListener("open", (evt) => {
-    messaging.peerSocket.send({});
-  });
-
-  messaging.peerSocket.addEventListener("message", (evt) => {
+  }
+  
+  handleMessage(evt) {
     if (evt.data['error']) {
-      showMessage(evt.data['error']);
+      this.showMessage(evt.data['error']);
     } else {
-      showResults(evt.data['data']);
+      this.showResults(evt.data['data']);
     }
-  });
+  }
 }
 
-initialize();
+const overview = new OverviewPage();
+
+messaging.peerSocket.addEventListener("open", () => overview.refresh());
+messaging.peerSocket.addEventListener("message", (evt) => {
+  if (evt.data['service']) {
+    console.log(evt.data);
+  } else {
+    overview.handleMessage(evt);
+  }
+});
--- fitbit/companion/index.js
+++ fitbit/companion/index.js
@@ -2,7 +2,11 @@ import * as messaging from "messaging";
 import { settingsStorage } from "settings";
 
 messaging.peerSocket.addEventListener("message", (evt) => {
-  makeRequest();
+  if (evt['service']) {
+    serviceLogRequest(evt['service']);    
+  } else {
+    overviewRequest();
+  }
 });
 
 function getBaseURL() {
@@ -13,24 +17,48 @@ function getPassword() {
   return JSON.parse(settingsStorage.getItem('password') || '{"name":""}')["name"];
 }
 
-function makeRequest() {
+function overviewRequest() {
   const url = getBaseURL() + '/api/overview?password=' + encodeURIComponent(getPassword())
   fetch(url, {
-        credentials: 'include',
-        redirect: 'manual',
+    credentials: 'include',
+    redirect: 'manual',
   }).then((x) => {
     return x.json();
   }).then((data) => {
     if (data['error']) {
-      messaging.peerSocket.send({error: data['error']});
+      messaging.peerSocket.send({service: null, error: data['error']});
     } else {
       let rows = [];
       data['data'].forEach((x) => {
         rows.push([x['serviceName'], x['message']]);
       });
-      messaging.peerSocket.send({data: rows});
+      messaging.peerSocket.send({service: null, data: rows});
     }
   }).catch((err) => {
-    messaging.peerSocket.send({error: 'Login error: ' + err});
+    messaging.peerSocket.send({service: null, error: 'Login error: ' + err});
+  });
+}
+
+function serviceLogRequest(serviceName) {
+  const url = getBaseURL() + '/api/serviceLog?password=' + encodeURIComponent(getPassword())
+  fetch(url, {
+    credentials: 'include',
+    redirect: 'manual',
+    method: 'POST',
+    body: JSON.stringify({service: serviceName}),
+  }).then((x) => {
+    return x.json();
+  }).then((data) => {
+    if (data['error']) {
+      messaging.peerSocket.send({service: serviceName, error: data['error']});
+    } else {
+      let rows = [];
+      data['data'].forEach((x) => {
+        rows.push(x['message']);
+      });
+      messaging.peerSocket.send({service: serviceName, data: rows});
+    }
+  }).catch((err) => {
+    messaging.peerSocket.send({service: serviceName, error: 'Login error: ' + err});
   });
 }
